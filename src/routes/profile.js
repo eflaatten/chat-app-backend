911f14de6f63c2a3f4bd6c011139d694df3fadac
eflaatten <elliotflaatten@live.com>
2024-11-25 12:04:42 -0600

    remove multer temp

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,15 +1,10 @@
 const express = require("express");
 const { uploadProfilePicture } = require("../controllers/profile");
 const { authenticateToken } = require("../middleware/authenticate");
-const multer = require("multer");
-const upload = multer();
+// const multer = require("multer");
+// const upload = multer();
 const router = express.Router();
 
-router.post(
-  "/uploadProfilePicture",
-  authenticateToken,
-  upload.single("imageUrl"),
-  uploadProfilePicture
-);
+router.post("/uploadProfilePicture", authenticateToken, uploadProfilePicture);
 
 module.exports = router;
